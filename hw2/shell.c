cc3a2588260759f09fe184da0d3cefaf4d02e996
Yuheng Shao <yshaoam@berkeley.edu>
2020-07-04 03:16:39 +0000

    first build

--- hw2/shell.c
+++ hw2/shell.c
@@ -30,6 +30,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -44,6 +46,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd,"pwd","print the current working directory"},
+  {cmd_cd,"cd","change to the specified directory"}
 };
 
 /* Prints a helpful description for the given command */
@@ -57,6 +61,23 @@ int cmd_help(unused struct tokens *tokens) {
 int cmd_exit(struct tokens *tokens) {
   tokens_destroy(tokens);
   exit(0);
+}
+
+int cmd_pwd(unused struct tokens *tokens){
+  printf("%s\n",getcwd(NULL,0));
+  return 1;
+}
+
+int cmd_cd(struct tokens *tokens){
+  if(tokens_get_length(tokens)>2){
+    printf("cd: too many arguments\n");
+    return -1;
+  }
+  if(chdir(tokens_get_token(tokens,1))==-1){
+    printf("cd : No such file or directory\n");
+    return -1;
+  }
+  else return 0;
 }
 
 /* Looks up the built-in command, if it exists. */
